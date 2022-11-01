c8472471d5c45d7045ba76147a1ac7f7b826d84e
Alexander Kaserbacher <alex.kaserbacher@icloud.com>
2022-11-01 09:51:34 +0100

    add demo node.js service

---
+++ demo-service/src/app.ts
@@ -0,0 +1,16 @@
+import express, { Application, Request, Response } from "express";
+import bodyParser from "body-parser";
+const app: Application = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.get("/", (req: Request, res: Response) => {
+  res.send("TS App is Running");
+});
+const PORT = 4000;
+app.listen(PORT, () => {
+  console.log(`server is running on PORT ${PORT}`);
+});
+
+app.get("/key", (req, res) => {
+  res.send({ key: "value" });
+});
