19ffbdbff3ea8ee1df05fb5c0051bf63af528fd3
Alexander Kaserbacher <alex.kaserbacher@icloud.com>
2022-11-03 17:30:21 +0100

    make experiment slower

--- load-testing/script.js
+++ load-testing/script.js
@@ -3,14 +3,14 @@ import { sleep } from 'k6';
 
 export const options = {
     stages: [
-        { duration: '10s', target: 1000 },
-        { duration: '10s', target: 2000 },
-        { duration: '10s', target: 3000 },
-        { duration: '10s', target: 4000 },
-        { duration: '10s', target: 5000 },
-        { duration: '10s', target: 6000 },
-        { duration: '10s', target: 7000 },
-        { duration: '10s', target: 8000 },
+        { duration: '20s', target: 1000 },
+        { duration: '20s', target: 2000 },
+        { duration: '20s', target: 3000 },
+        { duration: '20s', target: 4000 },
+        { duration: '20s', target: 5000 },
+        { duration: '20s', target: 6000 },
+        { duration: '20s', target: 7000 },
+        { duration: '20s', target: 8000 },
     ],
 };
 
